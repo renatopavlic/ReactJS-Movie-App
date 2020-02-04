3f975a79222eab9105c6510ac6e0ebf2bd6793f6
Renato Pavlić <renato.poslovni@gmail.com>
2020-02-04 20:25:57 +0100

    some styling to body and input tag

--- movie-app/src/components/Search.js
+++ movie-app/src/components/Search.js
@@ -3,7 +3,7 @@ import React from "react";
 function Search() {
     return (
         <section className="searchbox-wrap">
-            <input type="text" placeholder="Search for Movie" className="search-box"></input>
+            <input type="text" placeholder="Search for Movie" className="searchbox"></input>
         </section>
     )
 }
